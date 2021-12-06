171b254bce1eef5f95fccc70fe776bddd2e90b14
nyamzy <john.nyamweya@student.moringaschool.com>
2021-12-06 12:06:25 +0300

    test save multiple contacts and define tearDown method

--- password_locker_test.py
+++ password_locker_test.py
@@ -13,6 +13,11 @@ class TestUser(unittest.TestCase):
         '''
         self.new_user = User('John', 'Nyamweya', 'nyamzy', 'john@gmail.com', '1234') #Create new contact object
 
+    def tearDown(self):
+        '''
+        Tear down method that cleans up after each test has run
+        '''
+        User.users = []
 
     def test_init(self):
         '''
@@ -31,6 +36,16 @@ class TestUser(unittest.TestCase):
         '''
         self.new_user.save_user() #saves the new user
         self.assertEqual(len(User.users),1)
-         
+
+
+    def test_save_multiple_users(self):
+        '''
+        Test to check if we can save multiple users
+        '''
+        self.new_user.save_user()
+        test_user = User('Test', 'User', 'user', 'test@gmail.com', '1234')
+        test_user.save_user()
+        self.assertEqual(len(User.users),2)
+
 if __name__ == '__main__':
     unittest.main()
