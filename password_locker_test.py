7e0ba8937de300ebee94dff78f4bb5fde33c6cbe
nyamzy <john.nyamweya@student.moringaschool.com>
2021-12-06 11:29:08 +0300

    test to check if initialization of objects is done properly

--- password_locker_test.py
+++ password_locker_test.py
@@ -12,3 +12,19 @@ class TestUser(unittest.TestCase):
         Set up method that runs before each test case
         '''
         self.new_user = User('John', 'Nyamweya', 'nyamzy', 'john@gmail.com', '1234') #Create new contact object
+
+
+    def test_init(self):
+        '''
+        Test to check if the object is initialized properly
+        '''
+        self.assertEqual(self.new_user.first_name, 'John')
+        self.assertEqual(self.new_user.last_name, 'Nyamweya')
+        self.assertEqual(self.new_user.username, 'nyamzy')
+        self.assertEqual(self.new_user.email, 'john@gmail.com')
+        self.assertEqual(self.new_user.password, '1234')
+
+
+if __name__ == '__main__':
+    unittest.main()
+    
