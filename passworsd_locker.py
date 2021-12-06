c1b7b1a8a55eeed68f2d97bb90fb1dd6fea25bb2
nyamzy <john.nyamweya@student.moringaschool.com>
2021-12-06 10:51:56 +0300

    define the properties for our objects

---
+++ passworsd_locker.py
@@ -0,0 +1,14 @@
+class User:
+    '''
+    Class that generates new instances of users
+    '''
+
+    def __init__(self, first_name, last_name, username, email, password):
+        '''
+        init method helps to define the properties for our objects
+        '''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.username = username
+        self.email = email
+        self.password = password
